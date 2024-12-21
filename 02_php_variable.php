9a75d3ac97e6e0fc0e59c4d70126518376950477
tafhimbn <tafhim.aiub@gmail.com>
2024-12-21 21:52:55 +0600

    variables

---
+++ 02_php_variable.php
@@ -0,0 +1,17 @@
+<?php
+  // Creating (Declaring) PHP Variables
+           /* In PHP, a variable starts with the $ sign
+              A variable name cannot start with a number
+              Variable names are case-sensitive ($age and $AGE are two different variables)
+           */
+  $x = 5;
+  $y = "John";
+
+
+  // Output Variables
+         /* The PHP echo statement is often used to output data to the screen.
+         */
+
+   $nam = "Tafhim";
+     echo "I am $nam. <br>";  // <br> tag used for html pursing
+?>

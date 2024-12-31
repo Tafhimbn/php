8fde13d7f78d938b6b72c3a62639cd017cb98d01
tafhimbn <tafhim.aiub@gmail.com>
2024-12-31 22:00:12 +0600

    update

--- 13_mysql.php
+++ 13_mysql.php
@@ -3,16 +3,36 @@
     /* 
     Way to connect a MySQL Database:
         1. MySQLi extension (the "i" stands for improved)
+                i) MySQLi (object-oriented)
+                ii) MySQLi (procedural)
+
         2. PDO (PHP Data Objects)
 
+        -> PDO will work on 12 different database systems, whereas MySQLi will only work with MySQL databases.
     */
 
     // Connecting a Connection
+
     $server_name ="localhost";
     $username = "root";
     $password = "111";
 
-    // Create a Connection
+    /*
+    // ------------- Using MySQLi Object-Oriented ------------- 
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+    */
+
+    
+    //------------- Using MySQLi Procedural --------------------
+
     $conn = mysqli_connect($server_name, $username, $password);
 
     // Die if connection was not successful
@@ -22,5 +42,22 @@
     else {
         echo "<br> Connection was successful";
     }
+
+    // ------------- Using MySQLi Object-Oriented ------------- 
+    /*
+
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    }
+    
+    */
+
+
+
 
 ?>
